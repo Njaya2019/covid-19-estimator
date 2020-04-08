9985879f72544f5688e1ce7e99cafbda3556819b
Andrew <njayaandrew@gmail.com>
2020-04-08 23:27:00 +0300

    [chore] created best and worst case variables

--- src/estimator.py
+++ src/estimator.py
@@ -1,23 +1,35 @@
 def estimator(data):
-    bestCase = Covid19Cases.estimateImpactCurrentlyInfected(
+
+    # Best case
+
+    bestCase = Covid19Cases.estimateBestCase(
         data['reportedCases'])
+
+    # Worst case
+
+    worstCase = Covid19Cases.estimateWorstCase(
+        data['reportedCases'])
+
+    # Estimated best case after a specified time
+
+    bestCase_byTime = Covid19Cases.estimateCasesByTime(
+        data['timeToElapse'], bestCase)
+
+    # Estimated worst case after a specified time
+
+    worstCase_byTime = Covid19Cases.estimateCasesByTime(
+        data['timeToElapse'], worstCase)
 
     output_data = {
         "data": data,
         "impact": {
             "currentlyInfected": bestCase,
-            "infectionsByRequestedTime": Covid19Cases.estimateInfectionsByTime(
-                data['timeToElapse'], 
-                Covid19Cases.estimateImpactCurrentlyInfected(
-                    data['reportedCases']))},
-        "severeImpact": {
-            "currentlyInfected": Covid19Cases.estimateSevereCurrentlyInfected(
-                data['reportedCases']),
-            "infectionsByRequestedTime": Covid19Cases.estimateInfectionsByTime(
-                data['timeToElapse'], 
-                Covid19Cases.estimateSevereCurrentlyInfected(
-                    data['reportedCases'])),
+            "infectionsByRequestedTime": bestCase_byTime
         },
+        "severeImpact": {
+            "currentlyInfected": worstCase,
+            "infectionsByRequestedTime": worstCase_byTime
+        }
     }
     return output_data
 
@@ -27,23 +39,23 @@ def estimator(data):
 class Covid19Cases():
 
     @staticmethod
-    def estimateImpactCurrentlyInfected(reported_cases):
+    def estimateBestCase(reported_cases):
         """Estimates the possibly infected people"""
         return float(reported_cases * 10)
 
     @staticmethod
-    def estimateSevereCurrentlyInfected(reported_cases):
+    def estimateWorstCase(reported_cases):
         """Estimates the severe possibility of infected people"""
         return float(reported_cases * 50)
 
     @staticmethod
-    def estimateInfectionsByTime(days, currently_infected):
+    def estimateCasesByTime(days, currently_infected):
         """Estimates the number of infections in specified days"""
         # calculates the factor
 
         factor = int(days / 3)
 
-        # calculates the estimate of infections in the specified days
+        # calculates the estimate of infections after specified days
 
         infenctions_after_specified_days = currently_infected * (2**factor)
 
