39c2be778d396cbd673b27be2de5d03c032a31f1
Andrew <njayaandrew@gmail.com>
2020-04-09 01:33:50 +0300

    [feature] Added functionality to best and worst case severe cases and the number of available beds for severe cases

--- src/estimator.py
+++ src/estimator.py
@@ -27,15 +27,39 @@ def estimator(data):
     worstCase_byTime = Covid19Cases.estimateCasesByTime(
         data['timeToElapse'], worstCase)
 
+    # Estimated best case for severe cases after a period of time
+
+    bestCase_severeCases = Covid19Cases.estimatesevereCasesByRequestedTime(
+        bestCase_byTime)
+
+    # Estimated worst case for severe cases after a period of time
+
+    worstCase_severeCases = Covid19Cases.estimatesevereCasesByRequestedTime(
+        worstCase_byTime)
+
+    # Estimated best case available hospital beds for severe cases
+
+    bestCase_avalilableBeds = Covid19Cases.estimateAvailableHospitalBeds(
+        data["totalHospitalBeds"], bestCase_severeCases)
+
+    # Estimated worst case available hospital beds for severe cases
+
+    worstCase_avalilableBeds = Covid19Cases.estimateAvailableHospitalBeds(
+        data["totalHospitalBeds"], worstCase_severeCases)
+
     output_data = {
         "data": data,
         "impact": {
             "currentlyInfected": bestCase,
-            "infectionsByRequestedTime": bestCase_byTime
+            "infectionsByRequestedTime": bestCase_byTime,
+            "severeCasesByRequestedTime": bestCase_severeCases,
+            "hospitalBedsByRequestedTime": bestCase_avalilableBeds
         },
         "severeImpact": {
             "currentlyInfected": worstCase,
-            "infectionsByRequestedTime": worstCase_byTime
+            "infectionsByRequestedTime": worstCase_byTime,
+            "severeCasesByRequestedTime": worstCase_severeCases,
+            "hospitalBedsByRequestedTime": worstCase_avalilableBeds
         }
     }
 
@@ -72,3 +96,28 @@ class Covid19Cases():
         infenctions_after_specified_days = currently_infected * (2**factor)
 
         return float(infenctions_after_specified_days)
+
+    @staticmethod
+    def estimatesevereCasesByRequestedTime(infections_by_requested_time):
+        """Estimates the percentage of positive cases that are severe"""
+        severe_positive_cases = (15/100) * infections_by_requested_time
+        return severe_positive_cases
+
+    @staticmethod
+    def estimateAvailableHospitalBeds(hospital_beds, severe_positive_cases):
+        """Estimates hospital beds available for severe positive cases"""
+        # calculates 90% hospital beds capacity utilized
+
+        hospital_beds_utilized = (90/100) * hospital_beds
+
+        # calculates the hospital beds available for severe cases
+
+        available_hospital_beds = (35/100) * hospital_beds_utilized
+
+        # if severe cases are higher than available beds return a,
+        # negative number. Else return the available beds.
+
+        if(severe_positive_cases > available_hospital_beds):
+            return available_hospital_beds - severe_positive_cases
+        else:
+            return available_hospital_beds
