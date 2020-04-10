3848a4d5258774a7316550baee9bfedad0465f36
Andrew <njayaandrew@gmail.com>
2020-04-10 05:45:53 +0300

    [chore] modified the function estimating hospital beds available, excluded the hospital capacity

--- src/estimator.py
+++ src/estimator.py
@@ -175,9 +175,7 @@ class Covid19Cases():
 
         # calculates the beds occupied
 
-        capacity_beds = (95/100) * hospital_beds
-
-        occupied_beds = (65/100) * capacity_beds
+        occupied_beds = (65/100) * hospital_beds
 
         already_available_beds = hospital_beds - occupied_beds
 
