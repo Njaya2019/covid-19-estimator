1d32ae94fdd844be88ee1148638977e53c50158d
Andrew <njayaandrew@gmail.com>
2020-04-12 02:25:01 +0300

    [chore] Changed xml and json response

--- estimator_blueprint.py
+++ estimator_blueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, Response
 from form_validator import FormValidator
 from src.estimator import estimator
 from dicttoxml import dicttoxml
+from json import dumps
 
 # creates a blueprint object called estimate_blueprint
 
@@ -88,6 +89,10 @@ def estimator_endpoint(outputformat=None):
 
                 estimatedData = estimator(data)
 
+                # Converts the estimates into a json abject
+
+                json_estimates = dumps(estimatedData)
+
                 # If the output format variable is xml
 
                 if outputformat == "xml":
@@ -100,17 +105,44 @@ def estimator_endpoint(outputformat=None):
 
                     xml = estimatedData_xml.decode()
 
-                    return Response(xml, mimetype='text/xml')
+                    xml_response = Response(
+                        xml,
+                        content_type='application/xml; charset=utf-8'
+                    )
+                    xml_response.headers.add(
+                        'content-length', len(xml)
+                    )
+                    xml_response.status_code = 201
+
+                    return xml_response
 
                 # else return json format
 
                 elif outputformat == 'json':
 
-                    return jsonify(estimatedData), 201
+                    json_response = Response(
+                        json_estimates,
+                        content_type='application/json; charset=utf-8'
+                    )
+                    json_response.headers.add(
+                        'content-length', len(json_estimates)
+                    )
+                    json_response.status_code = 201
+
+                    return json_response
 
                 else:
 
-                    return jsonify(estimatedData), 201
+                    json_response = Response(
+                        json_estimates,
+                        content_type='application/json; charset=utf-8'
+                    )
+                    json_response.headers.add(
+                        'content-length', len(json_estimates)
+                    )
+                    json_response.status_code = 201
+
+                    return json_response
 
     return jsonify({
         "status": 200,
