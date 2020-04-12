162c21cd0cfaec13bc1fde64275288ba6e232942
Andrew <njayaandrew@gmail.com>
2020-04-12 04:36:18 +0300

    [chore] modified the endpoint url, changed the variable type to a string

--- estimator_blueprint.py
+++ estimator_blueprint.py
@@ -14,7 +14,7 @@ estimate_blueprint = Blueprint('estimate', __name__)
     '/api/v1/on-covid-19', methods=["GET", "POST"]
 )
 @estimate_blueprint.route(
-    '/api/v1/on-covid-19/<path:dataformat>', methods=["GET", "POST"]
+    '/api/v1/on-covid-19/<string:dataformat>', methods=["GET", "POST"]
 )
 def estimator_endpoint(dataformat=None):
 
